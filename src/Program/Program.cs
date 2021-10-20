9d1e6bb49b723aa5319a2f219cbe7403a0a31617
Maximiliano Benítez <maximiliano.benitez@correo.ucu.edu.uy>
2021-10-20 17:00:36 -0300

    Hasta ejercicio 3

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using CompAndDel.Pipes;
 using CompAndDel.Filters;
+using TwitterUCU;
+using CognitiveCoreUCU;
 
 namespace CompAndDel
 {
@@ -8,6 +10,26 @@ namespace CompAndDel
     {
         static void Main(string[] args)
         {
+            //Ejercicio 1
+            PictureProvider provider = new PictureProvider();
+            IPicture picture = provider.GetPicture(@"luke.jpg");
+
+            IFilter desaturar = new FilterGreyscale();
+            IFilter negativo = new FilterNegative();
+
+            IPipe filtro = new PipeNull();
+
+            IPipe filtroNegativo = new PipeSerial(negativo, filtro);
+            IPicture imagenFiltroNegativo = filtroNegativo.Send(picture);
+            provider.SavePicture(imagenFiltroNegativo, @"lukeToFiltroNegativo.jpg");
+            TwitterImage posteoNegativo = new TwitterImage();
+            posteoNegativo.PublishToTwitter("No entiendo nada", @"lukeToFiltroNegativo.jpg");
+            
+            IPipe filtroDesaturar = new PipeSerial(desaturar, filtroNegativo);
+            IPicture imagenFiltroDesaturar = filtroDesaturar.Send(picture);
+            provider.SavePicture(imagenFiltroDesaturar, @"lukeToFiltroDesaturar.jpg");
+            TwitterImage posteoDesaturar = new TwitterImage();
+            posteoDesaturar.PublishToTwitter("No entiendo nada 2.0", @"lukeToFiltroDesaturar.jpg");
         }
     }
 }
